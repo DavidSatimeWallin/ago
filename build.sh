446cd6e44d9ae0d210cd5cd0d8589d3681b2a4c1
David Satime Wallin <david@dwall.in>
2020-09-21 19:47:34 +0200

    new binaries
    Signed-off-by: David Satime Wallin <david@dwall.in>

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-GOARCH=amd64 GOOS=openbsd govvv build -ldflags="-s -w" -o binaries/ago-openbsd-amd64 ago.go
-GOARCH=386 GOOS=openbsd govvv build -ldflags="-s -w" -o binaries/ago-openbsd-i386 ago.go
-GOARCH=arm GOOS=openbsd govvv build -ldflags="-s -w" -o binaries/ago-openbsd-arm ago.go
-GOARCH=arm64 GOOS=openbsd govvv build -ldflags="-s -w" -o binaries/ago-openbsd-arm64 ago.go
+GOARCH=amd64 GOOS=openbsd govvv build -ldflags="-s -w" -o binaries/ago-openbsd-amd64 .
+GOARCH=386 GOOS=openbsd govvv build -ldflags="-s -w" -o binaries/ago-openbsd-i386 .
+GOARCH=arm GOOS=openbsd govvv build -ldflags="-s -w" -o binaries/ago-openbsd-arm .
+GOARCH=arm64 GOOS=openbsd govvv build -ldflags="-s -w" -o binaries/ago-openbsd-arm64 .
 
-GOARCH=amd64 GOOS=linux govvv build -ldflags="-s -w" -o binaries/ago-linux-amd64 ago.go
-GOARCH=386 GOOS=linux govvv build -ldflags="-s -w" -o binaries/ago-linux-i386 ago.go
-GOARCH=arm GOOS=linux govvv build -ldflags="-s -w" -o binaries/ago-linux-arm ago.go
-GOARCH=arm64 GOOS=linux govvv build -ldflags="-s -w" -o binaries/ago-linux-arm64 ago.go
+GOARCH=amd64 GOOS=linux govvv build -ldflags="-s -w" -o binaries/ago-linux-amd64 .
+GOARCH=386 GOOS=linux govvv build -ldflags="-s -w" -o binaries/ago-linux-i386 .
+GOARCH=arm GOOS=linux govvv build -ldflags="-s -w" -o binaries/ago-linux-arm .
+GOARCH=arm64 GOOS=linux govvv build -ldflags="-s -w" -o binaries/ago-linux-arm64 .
